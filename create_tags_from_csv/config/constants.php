9c1673312f92600147dad1e34d778861563100e2
Rogerio Batista <rogeriobatista@Rogerios-MacBook-Pro.local>
2020-07-12 14:22:17 -0300

    Constants

--- create_tags_from_csv/config/constants.php
+++ create_tags_from_csv/config/constants.php
@@ -1,6 +1,8 @@
 <?php
+// Application Config
 define('DEFAULT_LOGIN', 'admin');
 define('DEFAULT_PASSWORD', '123');
+// Mautic Config
 define('MAUTIC_USER', 'rogerio');
 define('MAUTIC_PASSWORD', 'rogerio123');
 define('MAUTIC_URL', 'https://lab.leoborlot.com.br');
